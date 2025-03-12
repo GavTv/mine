912cbbb76b3e93053b45da9d4f1e076a6a8380f1
Niks <ktm.softwear@gmail.com>
2025-03-12 23:55:10 +0300

    new5

---
+++ index2.js
@@ -0,0 +1,12 @@
+// // Функция открытия модального окна и отображения выбранного изображения
+// function openModal(img) {
+//     var modal = document.getElementById("modal");
+//     var modalImg = document.getElementById("modal-img");
+//     modal.style.display = "block";
+//     modalImg.src = img.src;
+//   }
+
+//   // Функция закрытия модального окна
+//   function closeModal() {
+//     var modal = document.getElementById("modal");
+//     modal.style.display = "none";
