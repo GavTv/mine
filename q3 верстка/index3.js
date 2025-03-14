5bcb334ac89459f509a6bad835445c5d56e0851c
Niks <ktm.softwear@gmail.com>
2025-03-14 22:53:48 +0300

    new13

--- q3 верстка/index3.js
+++ q3 верстка/index3.js
@@ -2,11 +2,9 @@ document.getElementById("playButton").addEventListener("click", function () {
   var audio = document.getElementById("myAudio");
   if (audio.paused) {
     audio.play();
-    this.textContent = "EX";
+    this.textContent = "Du'r";
   } else {
     audio.pause();
-    this.textContent = "EX";
+    this.textContent = " søt";
   }
 });
-
-
